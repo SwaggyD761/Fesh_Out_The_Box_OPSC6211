232645a4793217ebd5b856117a48a527c62e9be9
SwaggyD761 <70922045+SwaggyD761@users.noreply.github.com>
2023-05-02 11:25:11 +0200

    Update ManageSneakers.kt

--- Demo001_Opsc/app/src/main/java/com/example/demo001_opsc/ManageSneakers.kt
+++ Demo001_Opsc/app/src/main/java/com/example/demo001_opsc/ManageSneakers.kt
@@ -6,6 +6,42 @@ import android.os.Bundle
 class ManageSneakers : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_manage_sneakers)
+        setContentView(R.layout.activity_manage_
     }
+
+    // Get the RecyclerView from the layout
+    val recyclerView: RecyclerView = findViewById(R.id.sneakers_recycler_view)
+
+    // Set the layout manager and adapter for the RecyclerView
+    recyclerView.layoutManager = GridLayoutManager(this, 3)
+    recyclerView.adapter = SneakerAdapter(getSneakersFromDatabase())
+
+
+
 }
+
+class SneakerAdapter(private val sneakers: List<Sneaker>) : RecyclerView.Adapter<SneakerAdapter.ViewHolder>() {
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val sneakerImage: ImageView = itemView.findViewById(R.id.sneaker_image)
+        val sneakerName: TextView = itemView.findViewById(R.id.sneaker_name)
+        val sneakerPrice: TextView = itemView.findViewById(R.id.sneaker_price)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.sneaker_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val sneaker = sneakers[position]
+
+        // Set the sneaker image, name, and price in the corresponding views
+        holder.sneakerImage.setImageResource(sneaker.image)
+        holder.sneakerName.text = sneaker.name
+        holder.sneakerPrice.text = "$${sneaker.price}"
+    }
+
+    override fun getItemCount() = sneakers.size
+}
+
